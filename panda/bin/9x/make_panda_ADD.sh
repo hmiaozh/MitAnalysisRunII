ea6cb19779d02fd4ce1f5a7fd59deb54b3ac4966
GuillelmoGomezCeballos <ceballos@cern.ch>
2019-12-18 10:00:03 -0500

    new

---
+++ panda/bin/9x/make_panda_ADD.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ $# == 1 ] && [ $1 == 1016 ]; then
+
+./leptonic_2016.py root://xrootd.cmsaf.mit.edu//store/user/paus/pandaf/009/ADDMonoZ_ZtoLL_MD-1_d-4_TuneCUETP8M1_13TeV-pythia8+RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1+MINIAODSIM/58028DEC-9ECC-E611-9054-6CC2173BBE90.root 0 ADDMonoZ_MD_1_d_4_2016_0.root
+./leptonic_2016.py root://xrootd.cmsaf.mit.edu//store/user/paus/pandaf/009/ADDMonoZ_ZtoLL_MD-1_d-4_TuneCUETP8M1_13TeV-pythia8+RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1+MINIAODSIM/76AAA7EB-2CCD-E611-BA8B-6CC2173BC990.root 0 ADDMonoZ_MD_1_d_4_2016_1.root
+
+elif [ $# == 1 ] && [ $1 == 2016 ]; then
+./leptonic_2016.py root://xrootd.cmsaf.mit.edu//store/user/paus/pandaf/009/ADDMonoZ_ZtoLL_MD-2_d-4_TuneCUETP8M1_13TeV-pythia8+RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1+MINIAODSIM/B6AC2783-4FCE-E611-9B3A-008CFA197448.root 0 ADDMonoZ_MD_2_d_4_2016_0.root
+./leptonic_2016.py root://xrootd.cmsaf.mit.edu//store/user/paus/pandaf/009/ADDMonoZ_ZtoLL_MD-2_d-4_TuneCUETP8M1_13TeV-pythia8+RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1+MINIAODSIM/DCB54863-4CCE-E611-A6B6-008CFA165F5C.root 0 ADDMonoZ_MD_2_d_4_2016_1.root
+
+elif [ $# == 1 ] && [ $1 == 3016 ]; then
+./leptonic_2016.py root://xrootd.cmsaf.mit.edu//store/user/paus/pandaf/009/ADDMonoZ_ZToLL_MD-3_d-4_TuneCUETP8M1_13TeV-pythia8+RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1+MINIAODSIM/EE3BAB0B-A2D5-E611-AAD8-02163E019CDB.root      0 ADDMonoZ_MD_3_d_4_2016_0.root
+./leptonic_2016.py root://xrootd.cmsaf.mit.edu//store/user/paus/pandaf/009/ADDMonoZ_ZToLL_MD-3_d-4_TuneCUETP8M1_13TeV-pythia8+RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1+MINIAODSIM/AA214B1D-ADD6-E611-9A76-02163E01393B.root 0 ADDMonoZ_MD_3_d_4_2016_1.root
+./leptonic_2016.py root://xrootd.cmsaf.mit.edu//store/user/paus/pandaf/009/ADDMonoZ_ZToLL_MD-3_d-4_TuneCUETP8M1_13TeV-pythia8+RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1+MINIAODSIM/DC8436AD-ACD5-E611-92EE-02163E014260.root 0 ADDMonoZ_MD_3_d_4_2016_2.root
+
+fi
